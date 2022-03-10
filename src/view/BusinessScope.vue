40e80472727bf4feff7f28424a3e0034179d53dd
zhangaixia602 <zhangaixia602@163.com>
2022-03-10 10:32:12 +0800

    修改框架,便于扩展

--- src/view/BusinessScope.vue
+++ src/view/BusinessScope.vue
@@ -1,8 +1,19 @@
 <template>
+  <HeaderPage/>
   <div>业务领域</div>
+  <FooterPage/>
+  <ToTop/>
 </template>
 <script>
+import HeaderPage from '@/components/HeaderPage'
+import FooterPage from '@/components/FooterPage'
+import ToTop from '@/components/ToTop'
 export default {
-  name: 'BusinessScope'
+  name: 'BusinessScope',
+  components: {
+    'HeaderPage': HeaderPage,
+    'FooterPage': FooterPage,
+    'ToTop': ToTop
+  }
 }
 </script>
--- src/view/CasePage.vue
+++ src/view/CasePage.vue
@@ -1,8 +1,19 @@
 <template>
+  <HeaderPage/>
   <div>案例演示公司指南</div>
+  <FooterPage/>
+  <ToTop/>
 </template>
 <script>
+import HeaderPage from '@/components/HeaderPage'
+import FooterPage from '@/components/FooterPage'
+import ToTop from '@/components/ToTop'
 export default {
-  name: 'CasePage'
+  name: 'CasePage',
+  components: {
+    'HeaderPage': HeaderPage,
+    'FooterPage': FooterPage,
+    'ToTop': ToTop
+  },
 }
 </script>
