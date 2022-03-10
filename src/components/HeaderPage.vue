0f4a482a8d09137c4ee22d5328d7a0fb251623c6
zhangaixia602 <zhangaixia602@163.com>
2022-03-10 17:25:37 +0800

    修改头部代码

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -11,9 +11,9 @@
 <script>
 export default {
   name: 'HeaderPage',
-  props: ['scrollTop'],
   data () {
     return {
+       scrollTop: 0,
       routes: [
         {
           path: '/',
@@ -33,6 +33,14 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    handleScroll () {
+      this.scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
   }
 }
 </script>
