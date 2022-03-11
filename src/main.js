4afd9b53ef00ee1ce4f6f7c18a7528009ec7ab17
zhangaixia602 <zhangaixia602@163.com>
2022-03-11 10:36:12 +0800

    修改官网跳转重新打开新的窗口

--- src/main.js
+++ src/main.js
@@ -3,4 +3,6 @@ import Antd from 'ant-design-vue';
 import App from './App';
 import router from './router';
 import 'ant-design-vue/dist/antd.css';
+import './assets/css/common.css';
+import './assets/font/iconfont.css';
 createApp(App).use(Antd).use(router).mount("#app");
