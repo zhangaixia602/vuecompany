18fcd5766cd8fcb85bee579f857df92775adbe79
cwt1234 <974338217@qq.com>
2022-03-30 14:31:16 +0800

    提交

--- src/view/InsectPage.vue
+++ src/view/InsectPage.vue
@@ -3,6 +3,7 @@
 </template>
 <script>
 import * as Three from 'three'
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 let mixer = null
@@ -19,9 +20,10 @@ export default {
       let height = window.innerHeight
       this.renderer = new Three.WebGL1Renderer({antialias: true})
       this.renderer.setSize(width, height)
-      threeLoader.appendChild(this.renderer.domElement)
-
+      threeLoader.appendChild(this.renderer.domElement)      
+   
       this.scene = new Three.Scene()
+      this.setEnvMap("000");
       this.scene.background = new Three.Color(0xeeeeee)
 
       let light = new Three.HemisphereLight(0xbbbbff, 0x444422, 1.5)
@@ -32,7 +34,7 @@ export default {
       this.camera.lookAt(this.scene.position)
       this.controls = new OrbitControls(this.camera, this.renderer.domElement)
       let loader = new GLTFLoader()
-      loader.load('../../../static/models/Bee.glb', (gltf) => {
+      loader.load('../../../static/models/cb.gltf', (gltf) => {
         this.scene.add(gltf.scene)
         this.scene.scale.set(15, 15, 15)
         mixer = new Three.AnimationMixer(gltf.scene)
@@ -40,6 +42,13 @@ export default {
         AnimationAction.play()
       })
     },
+    setEnvMap(hdr) {
+    new RGBELoader().setPath("../../../static/gltf/").load(hdr + ".hdr", (texture) => {
+      texture.mapping = Three.EquirectangularReflectionMapping;
+      this.scene.background = texture;
+      this.scene.environment = texture;
+    });
+  },
     animate () {
       requestAnimationFrame(this.animate)
       this.renderer.render(this.scene, this.camera)
