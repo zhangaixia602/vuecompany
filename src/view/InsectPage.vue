1ed77249c7a5dd867dabc3160936d51db5e2195c
zhangaixia602 <zhangaixia602@163.com>
2022-03-31 17:25:18 +0800

    模型添加事件

--- src/view/InsectPage.vue
+++ src/view/InsectPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="threeLoader"></div>
+  <div id="threeLoader">
+  <div id="plant" :class="{show:this.isShow}">电表信息</div>
+  </div>
 </template>
 <script>
 import * as Three from 'three'
@@ -10,6 +12,11 @@ let mixer = null
 let clock = new Three.Clock()
 export default {
   name: 'InsectPage',
+  data () {
+    return {
+      isShow:false
+    }
+  },
   methods: {
     handleScroll () {
       this.scrollTop = document.body.scrollTop || document.documentElement.scrollTop
@@ -25,7 +32,20 @@ export default {
       this.scene = new Three.Scene()
       this.setEnvMap("004");
       this.scene.background = new Three.Color(0xeeeeee)
-
+      //添加小图标
+      //let geometry = new Three.BufferGeometry()
+      var texture = new Three.TextureLoader().load("../../../static/cameraRed.png");
+      // 创建精灵材质对象SpriteMaterial
+      let SpriteMaterial = new Three.SpriteMaterial({
+        color:0xff00ff,
+        map: texture,//设置精灵纹理贴图
+      });
+      let sprite = new Three.Sprite(SpriteMaterial);
+      this.scene.add(sprite);
+      // 控制精灵大小，比如可视化中精灵大小表征数据大小
+      sprite.scale.set(15, 15, 1); //// 只需要设置x、y两个分量就可以
+      sprite.position.set(190, 110, -106)
+      sprite.name = "电表"
       let light = new Three.HemisphereLight(0xbbbbff, 0x444422, 1.5)
       light.position.set(0, 1, 0)
       this.scene.add(light)
@@ -36,19 +56,34 @@ export default {
       let loader = new GLTFLoader()
       loader.load('../../../static/models/lc.gltf', (gltf) => {
         this.scene.add(gltf.scene)
-        this.scene.scale.set(15, 15, 15)
         mixer = new Three.AnimationMixer(gltf.scene)
         var AnimationAction = mixer.clipAction(gltf.animations[0])
         AnimationAction.play()
       })
+      window.addEventListener('click',this.clickModel,false);
     },
     setEnvMap(hdr) {
-    new RGBELoader().setPath("../../../static/gltf/").load(hdr + ".hdr", (texture) => {
-      texture.mapping = Three.EquirectangularReflectionMapping;
-      this.scene.background = texture;
-      this.scene.environment = texture;
-    });
-  },
+      new RGBELoader().setPath("../../../static/gltf/").load(hdr + ".hdr", (texture) => {
+        texture.mapping = Three.EquirectangularReflectionMapping;
+        this.scene.background = texture;
+        this.scene.environment = texture;
+      });
+    },
+    clickModel (event) {
+      let raycaster = new Three.Raycaster();
+      let mouse = new Three.Vector2();
+      mouse.x = ( event.clientX / window.innerWidth )*2 - 1;
+      mouse.y = -( event.clientY / window.innerHeight )*2 + 1;
+      raycaster.setFromCamera( mouse, this.camera );
+      let intersects = raycaster.intersectObjects( this.scene.children );
+      if(intersects[0]['object']['name']==="电表"){
+        if(this.isShow){
+          this.isShow=false
+        }else{
+          this.isShow=true
+        }
+      }
+    },
     animate () {
       requestAnimationFrame(this.animate)
       this.renderer.render(this.scene, this.camera)
@@ -63,3 +98,19 @@ export default {
   }
 }
 </script>
+<style scoped>
+#threeLoader{
+  position: relative;
+}
+#plant{
+  display:none;
+  font-size:2rem;
+  color:red;
+  position: absolute;
+  pointer-events: auto;
+  user-select: none;
+}
+#plant.show{
+  display:block;
+}
+</style>
