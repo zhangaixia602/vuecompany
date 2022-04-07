948179854c8da03ba5ea0e41de5170d73d27c0f6
zhangaixia602 <zhangaixia602@163.com>
2022-04-07 15:02:42 +0800

    添加场景背景图

--- src/view/FactoryPage.vue
+++ src/view/FactoryPage.vue
@@ -3,17 +3,13 @@
 </template>
 <script>
 import * as THREE from 'three'
-// import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 let scene=null;
 export default {
   name: 'FactoryPage',
   methods: {
-    handleScroll () {
-      this.scrollTop = document.body.scrollTop || document.documentElement.scrollTop
-    },
     initThree () {
       let threeLoader = document.getElementById('factory')
       let width = window.innerWidth //窗口宽
@@ -21,33 +17,33 @@ export default {
       this.renderer = new THREE.WebGL1Renderer({antialias: true})
       this.renderer.setSize(width, height)
       threeLoader.appendChild(this.renderer.domElement)
-
+      
       scene = new THREE.Scene()
-      scene.background = new THREE.Color(0xeeeeee)
+      let cubeTextureLoader = new THREE.CubeTextureLoader();
+			cubeTextureLoader.setPath( '/static/models/lc/' );
 
-      let light = new THREE.HemisphereLight(0xbbbbff, 0x444422, 1.5)
-      light.position.set(0,1, 0)
-      scene.add(light)
+			let textureCube = cubeTextureLoader.load( [ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ] );
+			textureCube.encoding = THREE.sRGBEncoding;
+      scene.background = textureCube;
+      this.setEnvMap("004");
+      
       this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 10000)
       this.camera.position.set(0, 0, 400)
       this.camera.lookAt(scene.position)
+      let light = new THREE.HemisphereLight(0xbbbbff, 0x444422, 1.5)
+      light.position.set(0, 1, 0)
+      scene.add(light)
       this.controls = new OrbitControls(this.camera, this.renderer.domElement)
-//    let loader = new STLLoader();
-//           loader.load('/static/models/lc.stl',function (geometry) {
-//              let material = new THREE.MeshLambertMaterial({
-//              color: 0xbbbbff,
-//             }); //材质对象Material
-//             let mesh = new THREE.Mesh(geometry, material); //网格模型对象Mesh
-//   scene.add(mesh); //网格模型添加到场景中 
-// })
-      let objLoader = new OBJLoader()
-      let mtlLoader = new MTLLoader()
-      mtlLoader.load('/static/models/fj.mtl', function(materials) {
-        objLoader.setMaterials(materials);
-        objLoader.load('/static/models/fj.obj', function(obj) {
-            scene.add(obj);
-        })
+      let objLoader = new GLTFLoader()
+      objLoader.load('/static/models/lc.gltf', function(glb) {
+        scene.add(glb.scene);
       })
+    },
+     setEnvMap(hdr) {
+      new RGBELoader().setPath("../../../static/gltf/").load(hdr + ".hdr", (texture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        scene.environment = texture;
+      });
     },
     animate () {
       requestAnimationFrame(this.animate)
