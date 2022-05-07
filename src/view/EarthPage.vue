8309db153599b5d62bc51b03be82c6040dbd6407
cwt1234 <974338217@qq.com>
2022-05-07 15:46:37 +0800

    提交

--- src/view/EarthPage.vue
+++ src/view/EarthPage.vue
@@ -5,15 +5,47 @@
 	export default {
 		name: "EarthPage",
 		mounted() {
-      console.log(Cesium)
-			const viewer = new Cesium.Viewer("modelContainer");
+      Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzZjQ4NDIwZS1jMDg5LTQ4NjQtOWY1MS1mMzRjZWNhMGJjOWIiLCJpZCI6OTE0NDYsImlhdCI6MTY1MTcxODUxNn0.BbaE7gmxMnoKzjUHjztR4HIqulxHFrHYzIx-BcsNUMw";
+			const viewer = new Cesium.Viewer("modelContainer",{
+        // geocoder: false,
+				// homeButton: false,
+				// sceneModePicker: true,
+				// baseLayerPicker: true,
+				// navigationHelpButton: false,
+				// animation: false,
+				// timeline: false,
+				// fullscreenButton: false,
+				// creditsDisplay: false,
+				// vrButton: false,
+				//selectionIndicator: false,//关闭绿色框
+				//infoBox:false//关闭焦点弹出框
+      });
+      //关闭logo
+      //限制缩放
+		 viewer.scene.screenSpaceCameraController.maximumZoomDistance = 2500;
+     const tileset = viewer.scene.primitives.add(
+     new Cesium.Cesium3DTileset({
+	  //  url:'/static/tileset.json',
+     url: Cesium.IonResource.fromAssetId(985439)
+    
 
-const tileset = viewer.scene.primitives.add(
-  new Cesium.Cesium3DTileset({
-	  url:'/static/tileset.json',
-    //url: Cesium.IonResource.fromAssetId(976768),
   })
 );
+      tileset.tileVisible.addEventListener(function(tile) { 
+
+     var content = tile.content;
+console.log(content)
+
+       var featuresLength = content.featuresLength; 
+
+      for (var i = 0; i < featuresLength; i++) { 
+
+        let  feature =content.getFeature(i)
+
+      }   
+
+ });
+
 (async () => {
   try {
     await tileset.readyPromise;
